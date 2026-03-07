a54c76aa462a83ae109f1be586580e6f74e9894a
Sktanisha <sanjidatanisha99@gmail.com>
2026-03-06 22:25:30 -0500

    routing

---
+++ controller/auth.controller.js
@@ -0,0 +1,9 @@
+exports.registrationController = (req, res)=>{
+    res.send("registration");
+}
+
+exports.loginController = (req, res)=>{
+    res.send("login done ");
+}
+
+//module.exports = registrationController;
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+//http://localhost:8080/
+app.use("/", require("./route"));
+
 
 app.listen(port, ()=>{
     console.log(`server is running on port ${port}`);
