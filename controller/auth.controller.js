1e6482962696719912fae1820461ea13c9e61360
Sktanisha <sanjidatanisha99@gmail.com>
2026-04-04 21:04:32 -0400

    registration

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,11 +1,18 @@
+const userModel = require("../model/user.model");
 const { asyncHandler } = require("../utils/asyncHandler");
 
-exports.registrationController = asyncHandler( (req, res)=>{
+exports.registrationController = asyncHandler(async(req, res)=>{
 
-    res.send(req.body);
+    //res.send(req.body);
    // let{name, email, password, phone} = req.body
-   //const {name, email, password, phone} = req.body
-
+   const {name, email, password, phone} = req.body;
+   const user = new userModel({
+    email,
+    name,
+    password,
+    phone,
+   });
+   await user.save();
 });
 
 exports.loginController = (req, res)=>{
