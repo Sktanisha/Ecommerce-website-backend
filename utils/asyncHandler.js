65ef59f95802076d45d9d0022a2d5b4a6b625b80
Sktanisha <sanjidatanisha99@gmail.com>
2026-04-05 23:34:09 -0400

    ///

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -1,7 +1,7 @@
 exports.asyncHandler = (fn)=>{
-    return (req, res, next)=>{
+    return async (req, res, next)=>{
         try{
-            fn(req, res, next);
+           await fn(req, res, next);
         }catch(error){
             next(error);
         }
--- utils/globalErrorHandler.js
+++ utils/globalErrorHandler.js
@@ -1,5 +1,18 @@
 const { apiResponse } = require("./apiResponse")
 
 exports.globalErrorHandler = (err, req, res, next )=>{
-    apiResponse(res, 500, "internal server error")
+    if (err.name === "ValidationError"){
+        let errors = {};
+        Object.keys(err.errors).forEach((key)=>{
+            errors[key] = err.errors[key].message;
+        });
+
+        apiResponse(res, 400, errors);
+    } else{
+        apiResponse(res, 400, err.message || "Something went wrong");
+    }
+    
 }
+
+
+//52:39 
