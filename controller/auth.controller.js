7a08217a83d6699787ed8368c168d2c2112d6786
Sktanisha <sanjidatanisha99@gmail.com>
2026-04-06 21:59:00 -0400

    ...

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,12 +1,21 @@
+const { sendEmail } = require("../helpers/sendEmail");
 const userModel = require("../model/user.model");
 const { apiResponse } = require("../utils/apiResponse");
 const { asyncHandler } = require("../utils/asyncHandler");
 const bcrypt = require('bcrypt');
+//const otpGenerator = require('otp-generator')
 
 exports.registrationController = asyncHandler(async(req, res)=>{
     //res.send(req.body);
    // let{name, email, password, phone} = req.body
     const {name, email, password, phone} = req.body;
+    //const otp = otpGenerator.generate(6, { 
+    //    digits: true,
+    //    upperCaseAlphabets: false, 
+     //   specialChars: false 
+    //});
+    return;
+    res.send(otp);
     // hash password
     const hashpassword =await bcrypt.hash(password, 12);
 
@@ -17,7 +26,7 @@ exports.registrationController = asyncHandler(async(req, res)=>{
     phone,
     });
     await user.save();
-
+    sendEmail(email);
     apiResponse(res, 201, "user created successfully!", user);
 });
 
